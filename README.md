106c2c800bd64924ffd51dc0a0a33b8119343681
steedalot <daniel@gaida.io>
2023-07-12 11:27:40 +0200

    API läuft - Update der Spezifikationen

--- README.md
+++ README.md
@@ -21,17 +21,38 @@ Sie gibt an, um welches Objekt bzw. um welches Dokument es sich handelt. Die **i
 Der Parameter **chat** ist ein _Array_, dass die folgenden Eigenschaften hat:
 
 Jedes Element des Arrays ist ein _Objekt_ (JSON), dass _Schlüssel: Wert_ Elemente enthält. Diese enthalten chronologisch den kompletten Verlauf des Chats.
-Chatnachrichten haben die _Rolle_ **system** oder **user**.
+Chatnachrichten haben die _Rolle_ **assistant** oder **user**.
 
 Beispiel:
 
 [
     {"role": "user", "content": "Wer bist du?"},
-    {"role": "system", "content": "Ich bin dein freundlicher Museumsführer!"}
+    {"role": "assistant", "content": "Ich bin dein freundlicher Museumsführer!"}
 ]
 
-Ist der Parameter **chat** nicht gesetzt, wird davon ausgegangen, dass ein neuer Chat begonnen wird.
+Ist der Parameter **chat** nicht gesetzt, wird davon ausgegangen, dass ein neuer Chat begonnen wird. Üblicherweise begrüßt einen der Assistent dann in der definierten Rolle.
 
 ### Reply
 
-Als Antwort kommt immer nur die aktuelle Antwort des Systems. Der Chat muss im Frontend zwischengespeichert werden.
+Als Antwort kommt ein JSON-Objekt mit der aktuellen Antwort des Systems. Der komplette Chat muss im **Frontend** zwischengespeichert werden.
+
+**message**
+
+Enthält die Antwort des Assistenten bzw. die Begrüßung zu Beginn
+
+
+**finish_reason**
+
+Der Grund, wieso die Antwort endet. Im Idealfall _stop_
+
+**tokens**
+
+Die Länge des letzten Prompts. Hiermit ist der komplette Chatverlauf gemeint, der jedesmal gesendet werden muss. Das Modell verträgt maximal 16000 Tokens.
+                
+**object**
+
+Gibt die Bezeichnung des aktuellen Objekts / Dokuments zurück, um das es geht (beispielsweise _Augustustatue von Primaporta_).
+
+**role**
+
+Gibt die Rolle zurück, in der der Assistent agiert (beispielsweise _Museumsführer_ oder _IT-Assistent_).
--- index.php
+++ index.php
@@ -5,15 +5,91 @@ $header = "Content-Type: application/json;charset=utf-8";
 
 ini_set('display_errors', 'On');
 
+$debug = true;
 
 require "rb.php";
+require "Url.php";
+require "OpenAi.php";
+
+
+use Orhanerday\OpenAi\OpenAi;
 
 R::setup('mysql:host='.MYSQL_HOST.';dbname='.MYSQL_DATABASE, MYSQL_USER, MYSQL_PASS);
+$open_ai = new OpenAi(OPENAI_API_KEY);
 
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 
-if (isset($data->key)) {
+if (isset($data->action) && isset($data->id)) {
+    
+    switch ($data->action) {
+
+        case "chat":
+
+
+            $message2 = R::load("message", $data->id);
+            if ((((int)$message2->chat) - 1000000) > 0) {
+                $message1 = R::load("message", ((int)$message2->chat) - 1000000);
+            }
+            else {
+                $answer = "Das System konnte diesen Chat nicht finden.";
+                $status = 404;
+            }
+
+            $messages = [
+                [
+                    "role" => "system",
+                    "content" => $message1->message  
+                ],
+                [
+                    "role" => "system",
+                    "content" => $message2->message
+                ]
+            ];
+            
+            if ($debug) {
+                print("Das Messages Array ohne Nachrichten vom Client:");
+                print_r($messages);
+            }
+
+            if (isset($data->chat)) {
+
+                foreach ($data->chat as $message) {
+                    $messages[] = get_object_vars($message);
+                }
+            
+                if ($debug) {
+                    print("Das Messages Array mit Nachrichten vom Client:");
+                    print_r($messages);
+                }
+            }
+
+            $chat = $open_ai->chat([
+                "model" => "gpt-3.5-turbo-16k",
+                "messages" => $messages
+            ]);
+
+            if ($debug) {
+                print_r($chat);
+            }
+
+            $reply_openai = json_decode($chat);
+
+            $json_reply = [
+                "message" => $reply_openai->choices[0]->message->content,
+                "finish_reason" => $reply_openai->choices[0]->finish_reason,
+                "tokens" => $reply_openai->usage->prompt_tokens,
+                "object" => $message2->user,
+                "role" => $message1->user
+            ];
+
+            $answer = json_encode($json_reply);
+            $status = 200;
+
+
+            
+
+    }
 
 
 
@@ -23,9 +99,11 @@ else {
     
     $status = 303;
     $answer = "<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv=\"Refresh\" content=\"0; URL='https://github.com/steedalot/phoenixapi'\">\n</head>\n<body></body>\n</html>";
-    http_response_code($status);
-    echo $answer;
+
 
 }
+
+http_response_code($status);
+echo $answer;
 
 ?>
